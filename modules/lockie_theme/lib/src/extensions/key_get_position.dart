c04ba9ae83252e1921db9e78352b2ce5adec4776
Nathaniel Dragusin <dragusinnathaniel@gmail.com>
2022-05-22 14:08:22 +0100

    update

--- modules/lockie_theme/lib/src/extensions/key_get_position.dart
+++ modules/lockie_theme/lib/src/extensions/key_get_position.dart
@@ -2,17 +2,32 @@ import 'package:flutter/material.dart';
 
 extension KeyGetPosition on GlobalKey<State> {
 
-  RelativeRect getPosition([Offset offset = Offset.zero]) {
-    final RenderBox button = currentContext!.findRenderObject()! as RenderBox;
+  RelativeRect getRelativeRect() {
+    final RenderBox widget = currentContext!.findRenderObject()! as RenderBox;
     final RenderBox overlay = Navigator.of(currentContext!).overlay!.context.findRenderObject()! as RenderBox;
     final RelativeRect position = RelativeRect.fromRect(
       Rect.fromPoints(
-        button.localToGlobal(Offset.zero, ancestor: overlay), 
-        button.localToGlobal(button.size.bottomRight(Offset.zero) + offset, ancestor: overlay)
+        widget.localToGlobal(Offset.zero, ancestor: overlay), 
+        widget.localToGlobal(widget.size.bottomRight(Offset.zero), ancestor: overlay)
       ),
-      offset & overlay.size
+      Offset.zero & overlay.size
     );
 
     return position;
+  }
+
+  Rect getRect() {
+    final widget = currentContext!.findRenderObject() as RenderBox;
+    final size = widget.size;
+    final position = widget.localToGlobal(Offset.zero);
+
+    return Rect.fromLTWH(position.dx, position.dy, size.width, size.height);
+  }
+
+  Offset getPosition() {
+    final widget = currentContext!.findRenderObject() as RenderBox;
+    final overlay = Navigator.of(currentContext!).overlay!.context.findRenderObject()! as RenderBox;
+
+    return widget.localToGlobal(Offset.zero, ancestor: overlay);
   }
 }
--- website/lib/landing/view/landing_view.dart
+++ website/lib/landing/view/landing_view.dart
@@ -134,233 +134,243 @@ class _LandingViewState extends State<LandingView> with TickerProviderStateMixin
             }
           }
         },
-        child: ListView(
+        child: SingleChildScrollView(
           controller: scrollController,
           physics: NeverScrollableScrollPhysics(),
-          children: [
-            SizedBox(height: size.height * 0.3),
-            Center(
-              child: Text('Lockie.',
-                style: TextStyle(
-                  fontSize: 100,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-            ),
-            InkWell(
-              child: LoopTransition(
-                duration: Duration(milliseconds: 1200),
-                builder: (animation, widget) 
-                => FadeTransition(
-                  opacity: animation.drive(Tween(begin: 0.2, end: 1)),
-                  child: widget,
-                ),
-                child: Icon(Icons.keyboard_arrow_down_rounded, size: 36)
-              ),
-              onTap: () async {
-                animateTo(1);
-                if(frame2Controller.isDismissed) {
-                  await frame2Controller.forward();
-                  await frame3Controller.forward();
-                }
-              },
-            ),
-            SizedBox(height: size.height * 0.4),
-            AdaptiveScreen(
-              wideScreen: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50),
-                child: Row(
-                  children: [
-                    Image.asset('assets/screenshot_panel.png',
-                      width: 350,
-                    ),
-                    SizedBox(width: 20),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                FadeTransition(
-                                  opacity: subtitle1Animation,
-                                  child: FadeTransition(
-                                    opacity: subtitleFadeAnimation,
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(top: 10),
-                                      child: Text('Posh. ', 
-                                        style: Theme.of(context).textTheme.headline2!
-                                          .copyWith(
-                                            color: Colors.white,
-                                            fontFamily: 'GreatVibes',
-                                            fontSize: 80
-                                          )
-                                      ),
-                                    ),
-                                  )
-                                ),
-                                FadeTransition(
-                                  opacity: subtitle2Animation,
-                                  child: FadeTransition(
-                                    opacity: subtitleFadeAnimation,
-                                    child: Text('Light.', 
-                                      style: Theme.of(context).textTheme.headline2!
-                                        .copyWith(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w100,
-                                        )
-                                    ),
-                                  )
-                                ),
-                              ],
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                FadeTransition(
-                                  opacity: descriptionAnimation,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      border: Border(
-                                        bottom: BorderSide(
-                                          width: 2,
-                                          color: Colors.white
-                                        )
-                                      )
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(bottom: 2),
-                                      child: Text('keeps your passwords safe.',
-                                        style: Theme.of(context).textTheme.headline2!
-                                          .copyWith(
-                                            color: Colors.white,
-                                          )
-                                      ),
-                                    ),
-                                  )
-                                ),
-                                SizedBox(height: 10),
-                                FadeTransition(
-                                  opacity: frame3Controller,
-                                  child: SlideTransition(
-                                    position: frame3ArrowOffsetAnimation,
-                                    child: InkWell(
-                                      child: Icon(Icons.keyboard_arrow_down_rounded, 
-                                        size: 36,
-                                        color: Colors.white,
-                                      ),
-                                      onTap: () => animateTo(2),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            )
-                          ]
-                        ),
-                      )
-                    )
-                  ]
-                )
-              ),
-              mobileScreen: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(50, 0, 50, 30),
-                    child: Image.asset('assets/screenshot_panel.png'),
+          child: Column(
+            children: [
+              SizedBox(height: size.height * 0.3),
+              Center(
+                child: Text('Lockie.',
+                  style: TextStyle(
+                    fontSize: 100,
+                    fontWeight: FontWeight.w300,
                   ),
-                ]
-              ),
-            ),
-            AdaptiveScreen(
-              wideScreen: Padding(
-                padding: const EdgeInsets.fromLTRB(50, 300, 0, 100),
-                child: Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: size.width * 0.5,
-                          child: Text('Download now or make your own',
-                            style: Theme.of(context).textTheme.headline2!
-                              .copyWith(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        SizedBox(
-                          width: size.width * 0.5,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 20),
-                            child: WebText(Strings.data['description']),
-                          )
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 20, left: 20),
-                          child: InkWell(
-                            child: Image.asset('assets/github.png',
-                              width: 200,
-                              color: Colors.white,
-                              colorBlendMode: BlendMode.srcIn,
-                            ),
-                            onTap: () => launchUrl(Uri.parse('https://github.com/nathanielxd/lockie')),
-                          ),
-                        ),
-                        Image.asset('assets/google-play.png',
-                          width: 300,
-                        ),
-                      ]
-                    ),
-                    Expanded(
-                      child: Image.asset('assets/illustration.png')
-                    )
-                  ],
                 ),
               ),
-              mobileScreen: Padding(
-                padding: const EdgeInsets.all(20),
+              InkWell(
+                child: LoopTransition(
+                  duration: Duration(milliseconds: 1200),
+                  builder: (animation, widget) 
+                  => FadeTransition(
+                    opacity: animation.drive(Tween(begin: 0.2, end: 1)),
+                    child: widget,
+                  ),
+                  child: Icon(Icons.keyboard_arrow_down_rounded, size: 36)
+                ),
+                onTap: () async {
+                  animateTo(1);
+                  if(frame2Controller.isDismissed) {
+                    await frame2Controller.forward();
+                    await frame3Controller.forward();
+                  }
+                },
+              ),
+              SizedBox(height: size.height * 0.4),
+              _buildTab2(context),
+              _buildTab3(size, context)
+            ]
+          ),
+        ),
+      )
+    );
+  }
+
+  Widget _buildTab2(BuildContext context) {
+    return AdaptiveScreen(
+      wideScreen: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50),
+        child: Row(
+          children: [
+            Image.asset('assets/screenshot_panel.png',
+              width: 350,
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Download now or make your own',
-                      style: Theme.of(context).textTheme.headline3!
-                        .copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 20),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: WebText(Strings.data['description'],
-                              style: TextStyle(fontSize: 14),
-                            )
-                          ),
-                          Expanded(
-                            child: Image.asset('assets/illustration.png')
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 20, left: 20),
-                      child: InkWell(
-                        child: Image.asset('assets/github.png',
-                          width: 200,
-                          color: Colors.white,
-                          colorBlendMode: BlendMode.srcIn,
+                    Row(
+                      children: [
+                        FadeTransition(
+                          opacity: subtitle1Animation,
+                          child: FadeTransition(
+                            opacity: subtitleFadeAnimation,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child: Text('Posh. ', 
+                                style: Theme.of(context).textTheme.headline2!
+                                  .copyWith(
+                                    color: Colors.white,
+                                    fontFamily: 'GreatVibes',
+                                    fontSize: 80
+                                  )
+                              ),
+                            ),
+                          )
                         ),
-                        onTap: () => launchUrl(Uri.parse('https://github.com/nathanielxd/lockie')),
-                      ),
+                        FadeTransition(
+                          opacity: subtitle2Animation,
+                          child: FadeTransition(
+                            opacity: subtitleFadeAnimation,
+                            child: Text('Light.', 
+                              style: Theme.of(context).textTheme.headline2!
+                                .copyWith(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w100,
+                                )
+                            ),
+                          )
+                        ),
+                      ],
                     ),
-                    Image.asset('assets/google-play.png',
-                      height: 80,
-                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        FadeTransition(
+                          opacity: descriptionAnimation,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  width: 2,
+                                  color: Colors.white
+                                )
+                              )
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(bottom: 2),
+                              child: Text('keeps your passwords safe.',
+                                style: Theme.of(context).textTheme.headline2!
+                                  .copyWith(
+                                    color: Colors.white,
+                                  )
+                              ),
+                            ),
+                          )
+                        ),
+                        SizedBox(height: 10),
+                        FadeTransition(
+                          opacity: frame3Controller,
+                          child: SlideTransition(
+                            position: frame3ArrowOffsetAnimation,
+                            child: InkWell(
+                              child: Icon(Icons.keyboard_arrow_down_rounded, 
+                                size: 36,
+                                color: Colors.white,
+                              ),
+                              onTap: () => animateTo(2),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
                   ]
                 ),
-              ),
+              )
             )
           ]
+        )
+      ),
+      mobileScreen: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(50, 0, 50, 30),
+            child: Image.asset('assets/screenshot_panel.png'),
+          ),
+        ]
+      ),
+    );
+  }
+
+  Widget _buildTab3(Size size, BuildContext context) {
+    return AdaptiveScreen(
+      wideScreen: Padding(
+        padding: const EdgeInsets.fromLTRB(50, 300, 0, 100),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: size.width * 0.5,
+                  child: Text('Download now or make your own',
+                    style: Theme.of(context).textTheme.headline2!
+                      .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  width: size.width * 0.5,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: WebText(Strings.data['description']),
+                  )
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20, left: 20),
+                  child: InkWell(
+                    child: Image.asset('assets/github.png',
+                      width: 200,
+                      color: Colors.white,
+                      colorBlendMode: BlendMode.srcIn,
+                    ),
+                    onTap: () => launchUrl(Uri.parse('https://github.com/nathanielxd/lockie')),
+                  ),
+                ),
+                Image.asset('assets/google-play.png',
+                  width: 300,
+                ),
+              ]
+            ),
+            Expanded(
+              child: Image.asset('assets/illustration.png')
+            )
+          ],
         ),
-      )
+      ),
+      mobileScreen: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Download now or make your own',
+              style: Theme.of(context).textTheme.headline3!
+                .copyWith(fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: WebText(Strings.data['description'],
+                      style: TextStyle(fontSize: 14),
+                    )
+                  ),
+                  Expanded(
+                    child: Image.asset('assets/illustration.png')
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20, left: 20),
+              child: InkWell(
+                child: Image.asset('assets/github.png',
+                  width: 200,
+                  color: Colors.white,
+                  colorBlendMode: BlendMode.srcIn,
+                ),
+                onTap: () => launchUrl(Uri.parse('https://github.com/nathanielxd/lockie')),
+              ),
+            ),
+            Image.asset('assets/google-play.png',
+              height: 80,
+            ),
+          ]
+        ),
+      ),
     );
   }
 }
